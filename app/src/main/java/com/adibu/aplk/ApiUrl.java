ce8971fb3887540fe6a6de6e42c8cd35c2e217f2
Krisna <krisnafawwastaqi@gmail.com>
2018-08-14 20:18:25 +0700

    API Image

--- app/src/main/java/com/adibu/aplk/ApiUrl.java
+++ app/src/main/java/com/adibu/aplk/ApiUrl.java
@@ -4,8 +4,9 @@ public class ApiUrl {
     private static final String ROOT_API_URL = "https://cilukbaa.000webhostapp.com/v1/api.php?apicall=";
 
     //Informasi
-    public static final String URL_CREATE_MSG = ROOT_API_URL + "c_info";  //POST , params: nip, isi, gambar_info(optional)
+    public static final String URL_CREATE_MSG = ROOT_API_URL + "c_info";  //POST , params: nip, isi, pic(optional)
     public static final String URL_READ_MSGS = ROOT_API_URL + "r_info"; //GET , no params
+    //r_info_nip&nip= (Menggantikan yg atasnya, parameter nip adalah nip user, jadi user hanya bisa membaca pesan untuk dia)
     public static final String URL_UPDATE_MSG = ROOT_API_URL + "updatemsg"; //POST , params: no, isi
     public static final String URL_DELETE_MSG = ROOT_API_URL + "deletemsg&no="; //GET , params: no
 
@@ -17,7 +18,5 @@ public class ApiUrl {
     public static final String URL_DELETE_USER = ROOT_API_URL + "deleteuser&no="; //GET , params: nip
 
     //Image
-    public static final String URL_CREATE_INFO_NEW = ROOT_API_URL + "up_img_info_create"; //POST , params: pic
     public static final String URL_CREATE_INFO_UP = ROOT_API_URL + "up_img_info_update&no="; //POST , params: pic, no
-    public static final String URL_READ_INFO_IMAGE = ROOT_API_URL + "r_img_info&no="; //GET , params: no
 }
